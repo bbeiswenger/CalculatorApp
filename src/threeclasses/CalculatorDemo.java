71fe3a44532386705040389c5332c92e02d5afed
barb <barb@10.30.16.155>
2012-03-03 12:24:15 -0600

    Added labels to Main method: Branch1 final working solution

--- src/threeclasses/CalculatorDemo.java
+++ src/threeclasses/CalculatorDemo.java
@@ -1,7 +1,8 @@
 package threeclasses;
 
 /**
- * Branch 1
+ * Branch 1 - Final working solution
+ * 
  * @author Barb Beiswenger
  */
 public class CalculatorDemo {
@@ -11,19 +12,19 @@ public class CalculatorDemo {
         Calculator calculator = new Calculator();
                  
         calculator.add(8, 4);
-        System.out.println(calculator.getNumOne() + " + " + calculator.getNumTwo() 
-                + " = " + calculator.getTotal());
+        System.out.println("Add: " + calculator.getNumOne() 
+                + " + " + calculator.getNumTwo() + " = " + calculator.getTotal());
          
         calculator.subtract(20, 10);
-        System.out.println(calculator.getNumOne() + " - " + calculator.getNumTwo() 
-                + " = " + calculator.getTotal());
+        System.out.println("Subtract: " + calculator.getNumOne() 
+                + " - " + calculator.getNumTwo() + " = " + calculator.getTotal());
         
         calculator.multiply(13, 3);
-        System.out.println(calculator.getNumOne() + " * " + calculator.getNumTwo() 
-                + " = " + calculator.getTotal());
+        System.out.println("Multiply: " + calculator.getNumOne() 
+                + " * " + calculator.getNumTwo() + " = " + calculator.getTotal());
 
         calculator.divide(25, 10);                
-        System.out.println(calculator.getNumOne() + " / " + calculator.getNumTwo() 
-                + " = " + calculator.getTotal());
+        System.out.println("Divide: " + calculator.getNumOne() 
+                + " / " + calculator.getNumTwo() + " = " + calculator.getTotal());
     }
 }
